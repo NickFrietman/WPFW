0f59777ec58501217f190240a3fdbbbf18353a90
NickFrietman <94540681+NickFrietman@users.noreply.github.com>
2024-09-30 09:32:04 +0200

    Add files via upload

---
+++ EFcore/IC5/IC5/Migrations/20240925211141_Eerste.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IC5.Migrations
+{
+    /// <inheritdoc />
+    public partial class Eerste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Studenten",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "Email", nullable: false),
+                    Grades_Capacity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Studenten", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teacher",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TeachingCourse = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "Email", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teacher", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudentTeacher",
+                columns: table => new
+                {
+                    StudentsId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TeachersId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentTeacher", x => new { x.StudentsId, x.TeachersId });
+                    table.ForeignKey(
+                        name: "FK_StudentTeacher_Studenten_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Studenten",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StudentTeacher_Teacher_TeachersId",
+                        column: x => x.TeachersId,
+                        principalTable: "Teacher",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudentTeacher_TeachersId",
+                table: "StudentTeacher",
+                column: "TeachersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentTeacher");
+
+            migrationBuilder.DropTable(
+                name: "Studenten");
+
+            migrationBuilder.DropTable(
+                name: "Teacher");
+        }
+    }
+}
---
+++ EFcore/IC5/IC5/Program.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Windows.Markup;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyContext context = new MyContext();
+
+            /* var builder = WebApplication.CreateBuilder(args);
+
+             // Add services to the container.
+
+             builder.Services.AddControllers();
+             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+             builder.Services.AddEndpointsApiExplorer();
+             builder.Services.AddSwaggerGen();
+
+             var app = builder.Build();
+
+             // Configure the HTTP request pipeline.
+             if (app.Environment.IsDevelopment())
+             {
+                 app.UseSwagger();
+                 app.UseSwaggerUI();
+             }
+
+             app.UseHttpsRedirection();
+
+             app.UseAuthorization();
+
+             app.MapControllers();
+
+             app.Run(); */
+        }
+    }
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public int DirectorId { get; set; }
+        public Director Director { get; set; }
+        public int Year { get; set; }
+        public ICollection<Review> Reviews { get; } = new List<Review>();
+
+        public Movie(string title, int year)
+        {
+            Title = title;
+            Year = year;
+        }
+    }
+    public class Director
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Director(string name)
+        {
+            Name = name;
+        }
+    }
+    public class Review
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public string UserName { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie? Movie { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Review(int rating, string description, string username)
+        {
+            Rating = rating;
+            Description = description;
+            UserName = username;
+            CreatedAt = DateTime.Now;
+        }
+    }
+    public class MyContext : DbContext
+    {
+        public DbSet<Review> Review { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Director> Director { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"C:\Users\Nick\Desktop\WPFW\API\API\API_Db.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    
+}
